2dec463e334baa0826185fac017c612cbc5347e1
Viktor Tusa <tusavik@gmail.com>
2014-05-10 15:41:01 +0200

    Added Dockerfile and setup file for syslog-ng devel env.

---
+++ precise/syslog-ng-devel-env/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# clone and compile syslog-ng
+
+setup_syslog-ng()
+{
+  mkdir /build
+  mkdir /install
+  mkdir /source
+
+  git clone https://github.com/balabit/syslog-ng /source
+
+  cd /source
+  ./autogen.sh
+
+  cd /build
+  /source/configure --prefix /install --enable-debug
+
+  make && make install
+}
+
+# setup tmux windows
+
+if [ ! -f /source/autogen.sh ]
+ then
+  setup_syslog-ng
+fi
+
+tmux new-session -s syslog-ng -n source -d "cd /source; bash -i"
+tmux new-window  -t syslog-ng -n build  "cd /build; bash -i"
+tmux new-window  -t syslog-ng -n install  "cd /install; bash -i"
+
+tmux attach -t syslog-ng
